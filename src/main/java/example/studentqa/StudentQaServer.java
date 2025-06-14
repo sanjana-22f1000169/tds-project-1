9a5a02bedc69dabecee70f5aa042d4d4fd220e7c
sanjana-22f1000169 <22f1000169@ds.study.iitm.ac.in>
2025-06-14 23:36:00 +0530

    Create StudentQaServer.java

---
+++ src/main/java/example/studentqa/StudentQaServer.java
@@ -0,0 +1,31 @@
+// src/main/java/com/example/studentqa/StudentQaServer.java
+package example.studentqa;
+
+import com.sun.net.httpserver.HttpServer;
+import example.studentqa.handler.QuestionHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class StudentQaServer {
+
+    private static final int PORT = 8080; 
+    private static final String CONTEXT_PATH = "/api";
+
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            QuestionHandler questionHandler = new QuestionHandler();
+            server.createContext(CONTEXT_PATH, questionHandler);
+            server.setExecutor(null); 
+            server.start();
+
+            System.out.println("Student QA Server started on port " + PORT);
+            System.out.println("Access API at http://localhost:" + PORT + CONTEXT_PATH);
+
+        } catch (IOException e) {
+            System.err.println("Error starting server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
